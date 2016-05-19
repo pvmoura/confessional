0fdcda6473e621d7954cbfdcac23f6ef2da323dd
Pedro Moura <pmavfmoura@gmail.com>
2016-05-19 02:03:01 -0400

    removed old code

--- oldCode/test.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-
-import sys
-import time
-sys.stdout.flush()
-sys.stdout.write("THIS IS THE PYTHON PROGRAM WORKING\n")
-sys.stdout.flush()
-time.sleep(2)
-print "THIS IS TESTING THE PRINT FUNCTION"
